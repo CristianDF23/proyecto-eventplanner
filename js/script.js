3902cc692d20e83b0a1b0f6af82ddbe641e4c855
CristianDF23 <cristian.eam85@gmail.com>
2023-09-08 01:02:29 -0300

    modificacion navbar

--- js/script.js
+++ js/script.js
@@ -8,3 +8,33 @@ $('.menu-toggle').click(function () {
   $(".nav").toggleClass("mobile-nav");
   $(this).toggleClass("is-active");
 });
+
+let navBar = document.getElementById('navBar');
+let text = document.getElementsByClassName('link');
+
+window.onscroll = () => {
+  if (screen.width > 721) {
+    if (document.documentElement.scrollTop > 50) {
+      for (let i = 0; i < text.length; i++) {
+        text[i].style.color = 'black';
+      }
+      navBar.style.background = 'white';
+    } else {
+      for (let i = 0; i < text.length; i++) {
+        text[i].style.color = 'white';
+      }
+      navBar.style.background = 'transparent';
+    }
+  } else if (screen.width < 721) {
+    if (document.documentElement.scrollTop > 50) {
+      navBar.style.background = 'white';
+    } else {
+      navBar.style.background = 'transparent';
+    }
+  }
+}
+
+
+
+
+
