4e2cba8d44bd422bcdb5189bc3725cab80e8a76e
CristianDF23 <cristian.eam85@gmail.com>
2023-10-10 15:08:10 -0300

    modificacion nav

--- js/script.js
+++ js/script.js
@@ -1,34 +1,55 @@
 AOS.init();
 
-let navBar = document.getElementById('navBar');
-let link = document.getElementsByClassName('link');
-let btnMenu = document.getElementById('btncheck');
-let logo = document.getElementById('logo')
+const navBar = document.getElementById('navBar');
+const link = document.getElementsByClassName('link');
+const btnMenu = document.getElementById('btncheck');
+const logo = document.getElementById('logo')
+let scrollBar = false
 
 window.onscroll = () => {
     if (window.innerWidth > 850) {
         if (document.documentElement.scrollTop === 0) {
+            scrollBar = false
             navBar.style.background = 'transparent'
             for (const elem of link) {
                 elem.style.color = 'white';
                 logo.style.filter = 'invert(100%)'
             }
+            navBar.style.opacity = '1'
         } else {
+            scrollBar = true
             navBar.style.background = 'white';
             for (const elem of link) {
                 elem.style.color = 'black';
             }
+            navBar.style.opacity = '1'
             logo.style.filter = 'none'
+            setTimeout(() => {
+                if (scrollBar) {
+                    scrollBar = false
+                    navBar.style.opacity = '.1'
+                }
+            }, 2000)
         }
     } else {
         if (document.documentElement.scrollTop === 0) {
+            scrollBar = false;
             navBar.style.background = 'transparent'
             btnMenu.style.color = 'white'
             logo.style.filter = 'invert(100%)'
+            navBar.style.opacity = '1'
         } else {
+            scrollBar = true
             navBar.style.background = 'white';
             btnMenu.style.color = 'black'
             logo.style.filter = 'none'
+            navBar.style.opacity = '1'
+            setTimeout(() => {
+                if (scrollBar) {
+                    scrollBar = false
+                    navBar.style.opacity = '.1'
+                }
+            }, 2000)
         }
     }
 }
