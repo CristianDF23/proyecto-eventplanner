afca60dc466ceccdc5764920058c71e315dd270b
CristianDF23 <cristian.eam85@gmail.com>
2023-10-11 15:44:10 -0300

    modificaciones en scrolleo barra nav

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
 AOS.init();
 
 const navBar = document.getElementById('navBar');
-const link = document.getElementsByClassName('link');
+const link = document.querySelectorAll('.link');
 const btnMenu = document.getElementById('btncheck');
 const logo = document.getElementById('logo')
+const navMobile = document.getElementById('navMobile')
 let scrollBar = false
+
+let navDesplegado = false;
+
+for (const elem of link) {
+    elem.addEventListener('click', () => {
+        navMobile.style.left = '-100%';
+        navDesplegado = false;
+    });
+}
+
+btnMenu.addEventListener('click', () => {
+    if (navDesplegado) {
+        navMobile.style.left = '-100%';
+        navDesplegado = false;
+    } else {
+        navMobile.style.left = '0';
+        navDesplegado = true;
+    }
+});
+
+
+
+
+
 
 window.onscroll = () => {
     if (window.innerWidth > 850) {
@@ -13,23 +38,25 @@ window.onscroll = () => {
             navBar.style.background = 'transparent'
             for (const elem of link) {
                 elem.style.color = 'white';
-                logo.style.filter = 'invert(100%)'
             }
-            navBar.style.opacity = '1'
+            logo.style.filter = 'invert(100%)'
+            navBar.style.height = '90px'
         } else {
             scrollBar = true
             navBar.style.background = 'white';
             for (const elem of link) {
                 elem.style.color = 'black';
             }
-            navBar.style.opacity = '1'
+            navBar.style.height = '90px'
+            logo.style.display = 'block'
             logo.style.filter = 'none'
             setTimeout(() => {
                 if (scrollBar) {
                     scrollBar = false
-                    navBar.style.opacity = '.1'
+                    navBar.style.height = '0px'
+                    logo.style.display = 'none'
                 }
-            }, 2000)
+            }, 3000)
         }
     } else {
         if (document.documentElement.scrollTop === 0) {
@@ -37,19 +64,25 @@ window.onscroll = () => {
             navBar.style.background = 'transparent'
             btnMenu.style.color = 'white'
             logo.style.filter = 'invert(100%)'
-            navBar.style.opacity = '1'
+            btnMenu.style.display = 'block'
         } else {
             scrollBar = true
             navBar.style.background = 'white';
+            navBar.style.height = '90px'
+            logo.style.display = 'block'
             btnMenu.style.color = 'black'
             logo.style.filter = 'none'
-            navBar.style.opacity = '1'
+            btnMenu.style.display = 'block'
             setTimeout(() => {
                 if (scrollBar) {
-                    scrollBar = false
-                    navBar.style.opacity = '.1'
+                    scrollBar = false;
+                    navBar.style.height = '0px'
+                    logo.style.display = 'none'
+                    btnMenu.style.display = 'none'
                 }
-            }, 2000)
+            }, 3000)
         }
     }
+    console.log(scrollBar)
 }
+
