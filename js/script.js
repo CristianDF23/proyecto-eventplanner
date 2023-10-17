b5490cd62b237483184a3ec575bb684c1e88a419
CristianDF23 <cristian.eam85@gmail.com>
2023-10-16 21:47:00 -0300

    navbar

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const link = document.querySelectorAll('.link');
 const btnMenu = document.getElementById('btncheck');
 const logo = document.getElementById('logo')
 const navMobile = document.getElementById('navMobile')
-let scrollBar = false
 
 let navDesplegado = false;
 
@@ -27,62 +26,35 @@ btnMenu.addEventListener('click', () => {
 });
 
 
-
-
-
-
 window.onscroll = () => {
     if (window.innerWidth > 850) {
         if (document.documentElement.scrollTop === 0) {
-            scrollBar = false
+            navBar.classList.remove('navScroll')
             navBar.style.background = 'transparent'
             for (const elem of link) {
                 elem.style.color = 'white';
             }
             logo.style.filter = 'invert(100%)'
-            navBar.style.height = '90px'
         } else {
-            scrollBar = true
+            navBar.classList.add('navScroll')
             navBar.style.background = 'white';
             for (const elem of link) {
                 elem.style.color = 'black';
             }
-            navBar.style.height = '90px'
-            logo.style.display = 'block'
             logo.style.filter = 'none'
-            setTimeout(() => {
-                if (scrollBar) {
-                    scrollBar = false
-                    navBar.style.height = '0px'
-                    logo.style.display = 'none'
-                }
-            }, 3000)
         }
     } else {
         if (document.documentElement.scrollTop === 0) {
-            scrollBar = false;
+            navBar.classList.remove('navScroll')
             navBar.style.background = 'transparent'
             btnMenu.style.color = 'white'
             logo.style.filter = 'invert(100%)'
-            btnMenu.style.display = 'block'
         } else {
-            scrollBar = true
+            navBar.classList.add('navScroll')
             navBar.style.background = 'white';
-            navBar.style.height = '90px'
-            logo.style.display = 'block'
             btnMenu.style.color = 'black'
             logo.style.filter = 'none'
-            btnMenu.style.display = 'block'
-            setTimeout(() => {
-                if (scrollBar) {
-                    scrollBar = false;
-                    navBar.style.height = '0px'
-                    logo.style.display = 'none'
-                    btnMenu.style.display = 'none'
-                }
-            }, 3000)
         }
     }
-    console.log(scrollBar)
 }
 
